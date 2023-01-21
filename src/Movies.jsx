6441f55412a72cedca65ffeeb810bbb6c1405d81
balabala <balabba112@gmail.com>
2023-01-22 01:04:12 +0530

    addparagraph

---
+++ src/Movies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Movie1(){
+    return(
+        <ol>
+        <li>Avatar</li>
+        <li>Boss</li>
+        <li>Antman</li>
+      </ol>  
+    )
+}
+export default Movie1;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Movie1 from './Movies'
+
+ReactDOM.render(
+<>
+<h1>This is my Heading</h1>
+<p>my favorate movie</p>
+<p>changes 4</p>
+<h2> heros</h2>
+<Movie1></Movie1>
+<div> this is Movies</div>
+
+</>,
+document.getElementById("root"));
